9908cf45651041d7ecd154c616b9602f87e62741
Sanket Sharma <sanketsharma411@gmail.com>
2015-08-31 14:24:12 -0400

    added to do for USDA

--- flask_app/nutrition.py
+++ flask_app/nutrition.py
@@ -15,3 +15,7 @@ def nutrient_tags(tags):
     # Use flask.requests inplace of URL
     result = {str('%s %s'%(k[0],k[1])):v for k,v in USDA.id2nut(USDA.tag2id(str(tags))[0]).items()}
     return jsonify(result)
+    
+    
+# Maybe add tag2nut function inside USDA and implement it with default / ones in paper selection settings
+# and return in a jsonifiable format i.e. keys = string instead of tuples    
